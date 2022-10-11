4a96b1c64108e68af144e1e6f7bf7126f14f26e2
xchopath <novran.faathir@gmail.com>
2022-10-11 13:08:04 +0700

    1# commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# tokex.sh
+
+Tokex is a simple bug-bounty tool for auto recon and hunting Secret Key in Web Files.
+
+# Installation
+
+1. Install gau
+```
+go install -v github.com/lc/gau/v2/cmd/gau@latest
+```
+
+2. Install nuclei
+```
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+```
+
+3. Install tokex.sh
+```
+git clone https://github.com/xchopath/tokex.sh
+```
+
+4. Move to /usr/bin/
+```
+cd tokex.sh
+ln -sf $(pwd)/tokex.sh /usr/bin/tokex.sh
+```
+
+# Setting
+
+Nuclei and gau binary in `tokex.sh`
+```
+NUCLEI_BIN="${HOME}/go/bin/nuclei"
+GAU_BIN="${HOME}/go/bin/gau"
+```
+
+# Usage
+```
+tokex.sh www.example.com
+```
---
+++ tokex.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# tokex.sh
+# Made by @xchopath
+
+NUCLEI_BIN="${HOME}/go/bin/nuclei"
+GAU_BIN="${HOME}/go/bin/gau"
+
+TARGET_FQDN="${1}"
+FILE_TEMP="/tmp/tokexurls.$(date +%s).${RANDOM}.tmp"
+
+BANNER='''
+  _        _             
+ | |_ ___ | | _______  __
+ | __/ _ \| |/ / _ \ \/ /
+ | || (_) |   <  __/>  < 
+  \__\___/|_|\_\___/_/\_\
+  Token Exposure Scanner
+'''
+echo "${BANNER}"
+
+if [[ -z ${TARGET_FQDN} ]]; then
+	echo " Usage:"
+	echo "   $ tokex.sh www.example.com"
+fi
+
+if [[ -f ${FILE_TEMP} ]]; then
+	rm ${FILE_TEMP}
+	touch ${FILE_TEMP}
+fi
+
+TARGET_HTTP="$(curl -Ls -o /dev/null -w %{url_effective} ${TARGET_FQDN})"
+echo "${TARGET_HTTP}" >> ${FILE_TEMP}
+
+# Run GAU
+echo "${TARGET_FQDN}" | ${GAU_BIN} | sort -V | uniq >> ${FILE_TEMP}
+
+# Run Nuclei
+for URL in $(cat ${FILE_TEMP})
+do
+	${NUCLEI_BIN} -t exposures/tokens/ -u "${URL}" -silent
+done
+
+if [[ -f ${FILE_TEMP} ]]; then
+	rm ${FILE_TEMP}
+fi
+
+echo " DONE!"
